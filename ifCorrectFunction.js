249822dde2ffd024295b428e60e7c0e0839e5d4a
Leyber91 <leyber.91.2@gmail.com>
2020-11-26 21:03:08 +0100

    catch random word + select letter from word

---
+++ ifCorrectFunction.js
@@ -0,0 +1,23 @@
+let test = 'witch'
+let selectedWord = [test];
+let emptyString = [];
+
+/*if letter selected  matches  selectedWord  content */
+/*display identified letter = visible*/
+/*divide word into letters */
+/* put arrays of letters into another variable */
+/*for each of each letter in the variable
+/*appendchild to the parent element*/
+/*add a class to the parent element*/
+/*append child with styles*/
+
+/*ClassList*/
+
+/* Select an array and split it into elements */
+
+let splitted = selectedWord.split(" ");
+for (let i = 0; i < splitted.length; i++) {
+    emptyString.push(splitted[i])
+    console.log(emptyString)
+}
+
