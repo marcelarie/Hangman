d54009ac5380cdaf2b7ac31b22851bff7316ce2b
Marcel <googlillo@gmail.com>
2020-11-27 00:33:47 +0100

    Adding drawn on count function

--- canvas.js
+++ canvas.js
@@ -1,34 +1,57 @@
-window.addEventListener('load', () => {
-    const canvas = document.querySelector('#canvasBox');
-    const ctx = canvas.getContext('2d');
+const canvas = document.querySelector('#canvasBox');
+const ctx = canvas.getContext('2d');
 
-    canvas.height = 300;
-    canvas.width = 800;
+canvas.height = 300;
+canvas.width = 800;
 
-    //top stick
-    ctx.fillRect(150, 10, 130, 7);
-    //big stick
-    ctx.fillRect(150, 10, 7, 290);
-    // floor
-    ctx.fillRect(0, 295, 310, 7);
-    // rope
-    ctx.fillRect(277, 10, 4, 50);
-    // head
-    ctx.beginPath();
-    ctx.arc(280, 90, 30, 0, 2 * Math.PI);
-    ctx.stroke();
-    // body
-    ctx.fillRect(278, 120, 5, 80);
-    // right arm
-    ctx.rotate(10 * Math.PI / 180);
-    ctx.fillRect(290, 70, 5, 50);
-    // left arm
-    ctx.rotate(10 * Math.PI / -90);
-    ctx.fillRect(256, 167, 5, 50);
-    // right leg
-    ctx.rotate(10 * Math.PI / 180);
-    ctx.fillRect(278, 200, 5, 60);
-    // left leg
-    ctx.rotate(10 * Math.PI / -85);
-    ctx.fillRect(188, 285, 5, 60);
+let count = 0;
+
+hangManLetters.forEach(x => {
+    x.addEventListener('click', () => {
+        count++;
+        switch (count) {
+            case 1:
+                // 1. floor
+                ctx.fillRect(0, 295, 310, 7);
+                break;
+            case 2:
+                // 2. big stick
+                ctx.fillRect(150, 10, 7, 290);
+                break;
+            case 3:
+                // 3. top stick
+                ctx.fillRect(150, 10, 130, 7);
+                break;
+            case 4:
+                // 4. rope
+                ctx.fillRect(277, 10, 4, 50);
+                break;
+            case 5:
+                // 5. head
+                ctx.beginPath();
+                ctx.arc(280, 90, 30, 0, 2 * Math.PI);
+                ctx.stroke();
+                break;
+            case 6:
+                // 6. body
+                ctx.fillRect(278, 120, 5, 80);
+                break;
+            case 7:
+                // 7. right arm
+                ctx.fillRect(280, 120, 50, 4);
+                break;
+            case 8:
+                // 8. left arm
+                ctx.fillRect(230, 120, 50, 4);
+                break;
+            case 9:
+                // 9. right leg
+                ctx.fillRect(233, 200, 50, 4);
+                break;
+            case 10:
+                // 10. left leg
+                ctx.fillRect(278, 200, 5, 50);
+                break;
+        }
+    })
 })
