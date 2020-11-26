e83efe1ca8a9356f7327bd027315610db518d244
Mike <miguelgc95@gmail.com>
2020-11-26 18:59:24 +0100

    Show and hide screens and store name user

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("secondMainPage").classList.add("hide-me");
+document.getElementById("finalPage").classList.add("hide-me");
+
+function prevent(event) {
+    event.preventDefault();
+    var name=document.getElementById("userName").value;
+    document.getElementById("welcome").classList.add("hide-me");
+    document.getElementById("secondMainPage").classList.remove("hide-me");
+}
+
+document.getElementById("startButton").addEventListener("click", prevent);
