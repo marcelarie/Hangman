593a4f09b9940fbea2f6086984af8b7b26592558
Mike <miguelgc95@gmail.com>
2020-11-28 10:39:26 +0100

    Flow between gameScrren and finalScreen

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ document.getElementById("secondMainPage").classList.add("hide-me");
 document.getElementById("finalPage").classList.add("hide-me");
 document.getElementById("scores").classList.add("hide-me");
 
+var start;
+var end;
+var score;
+
 function prevent(event) {
     event.preventDefault();
+    start=Date.now();
     var name = document.getElementById("userName").value;
     document.getElementById("welcome").classList.add("hide-me");
     document.getElementById("secondMainPage").classList.remove("hide-me");
--- winOrLoss.js
+++ winOrLoss.js
@@ -1,6 +1,6 @@
 var level = 1;
 var letterCount = 0;
-var reference = '';
+var reference = 'adrhathatrhartrt';
 var userWord = '';
 
 
@@ -13,11 +13,21 @@ function incrementLetterCount(event) {
 
 function winOrLoss() {
     if (reference === userWord) {
+        document.getElementById("secondMainPage").classList.add("hide-me");
+        document.getElementById("scores").classList.remove("hide-me");
+        document.getElementById("finalPage").classList.remove("hide-me");
         var win = true;
         level += 1;
+        end=Date.now();
+        score=(end-start)/1000;
     }
     else if (letterCount === 10) {
+        document.getElementById("secondMainPage").classList.add("hide-me");
+        document.getElementById("scores").classList.remove("hide-me");
+        document.getElementById("finalPage").classList.remove("hide-me");
         win = false;
+        end=Date.now();
+        score=(end-start)/1000;
     }
 }
 
